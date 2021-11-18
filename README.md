684f076f6a0959532aba307e0843057ff1fcb32c
Dvin <dvyu@yandex.ru>
2021-11-18 23:48:11 +0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # sirius-docker-compose-deploy
+Apply all docker-compose yamls to docker host
+# example of usage in pipeline
+```
+name: Deploy Dev VM1
+# Restarts all changed docker containers in 'vm1' folder
+
+on:
+  push:
+    branches: [ master ]
+    paths:
+    - 'vm1/**'
+
+jobs:
+  deploy:
+    name: deploy
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - name: Define environment variables
+      run: cat common-environment-variables.sh >> $GITHUB_ENV
+    - name: Print environment variables
+      run: |
+        for varname in $(awk -F= '{print $1}' common-environment-variables.sh)
+        do
+            printf "%s: %s\n" $varname ${!varname}
+        done
+    - name: Restart docker container on VM
+      uses: swisschain/sirius-docker-compose-deploy@master
+      env:
+        ACTION: 'START'
+        DOCKER_VM_HOST: ${{ env.DOCKER_VM_HOST_1 }}
+        REPOSITORY_PATH_INFRASTRUCTURE: 'sirius-infra/docker-compose/vm1'
+        REPOSITORY_PATH_SECRETS: 'sirius-infra-secrets'
+        REPOSITORY_SERVICE_DIR: 'sirius/integrations/ethereum'
+        SSH_PRIVATE_KEY: ${{ secrets.SSH_PRIVATE_KEY }}
+        SSH_KNOW_HOST: ${{ env.SSH_KNOW_HOST_1 }}
+```
---
+++ entrypoint.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+
+# exit when any command fails
+set -e
+
+echo create ssh key
+echo $SSH_PRIVATE_KEY | base64 -d > /tmp/id_rsa
+chmod 400 /tmp/id_rsa
+echo create ssh know host file
+echo $SSH_KNOW_HOST > /tmp/known_hosts
+cat /tmp/known_hosts
+echo run command
+echo DOCKER_VM_HOST=$DOCKER_VM_HOST
+ssh $DOCKER_VM_HOST -i /tmp/id_rsa -o UserKnownHostsFile=/tmp/known_hosts << EOF
+  hostname
+  # define functions
+  start_docker() {
+    echo run service
+    docker-compose pull
+    docker-compose up -d
+    echo sleep for 2 seconds
+    sleep 2
+    echo print last logs
+    docker-compose logs --tail 100
+  }
+  stop_docker() {
+    echo stop service
+    docker-compose stop
+  }
+  down_docker() {
+    echo delete service
+    docker-compose down
+  }
+  restart_docker() {
+    echo restart service
+    docker-compose restart
+  }
+  echo pull secrets repository
+  cd $REPOSITORY_PATH_SECRETS/
+  git pull
+  if [ -f ./secrets.json ];then
+    echo found common secrets file
+    ls -la ./secrets.json
+  fi
+  echo pull main repository
+  cd ../$REPOSITORY_PATH_INFRASTRUCTURE
+  git pull
+  # if "$REPOSITORY_SERVICE_DIR" found then we apply to one component, otherwise to all components
+  if [ -n "$REPOSITORY_SERVICE_DIR" ];then
+    echo REPOSITORY_SERVICE_DIR defined - $REPOSITORY_SERVICE_DIR
+    DCD=$REPOSITORY_SERVICE_DIR
+  else
+    echo searching for docker-compose.yaml files
+    DCD=\$(find . -name docker-compose.yaml | awk -F/ '{print \$2}')
+  fi
+  echo list of dirs \$DCD
+  for DIR_NAME in \$DCD
+  do
+    echo
+    echo   - = [ \$DIR_NAME ] = -
+    if [ -d \$DIR_NAME ];then
+      cd \$DIR_NAME
+      pwd
+      # create a symlink for infra components to .env file (instead of coping it)
+      if [ -f ../../../$REPOSITORY_PATH_SECRETS/\$DIR_NAME/.env ];then
+        echo found .env file
+        if [ -f .env ];then
+          echo file or symlink exist
+        else
+          echo create symlink
+          ln -s ../../../$REPOSITORY_PATH_SECRETS/\$DIR_NAME/.env ./.env
+        fi
+      fi
+      # show if we found 'secrets.json'
+      if [ -f ../../../$REPOSITORY_PATH_SECRETS/\$DIR_NAME/secrets.json ];then
+        echo found service secrets file
+        ls -la ../../../$REPOSITORY_PATH_SECRETS/\$DIR_NAME/secrets.json
+      fi
+      ls -la
+      if [ "$ACTION" = "START" ];then
+        start_docker;
+      fi
+      if [ "$ACTION" = "STOP" ];then
+        stop_docker;
+      fi
+      if [ "$ACTION" = "DOWN" ];then
+        down_docker;
+      fi
+      if [ "$ACTION" = "RESTART" ];then
+        restart_docker;
+      fi
+    else
+      echo \$DIR_NAME doesn\'t exist
+    fi
+    cd ..
+  done
+  echo remove orphan docker images
+  docker image prune -af
+EOF
+# remove private key
+rm /tmp/id_rsa
