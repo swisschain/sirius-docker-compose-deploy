6115be4ef4c5d32ca4d7158e1b12c83732bdcdcd
Dvin <dvyu@yandex.ru>
2021-12-07 10:25:58 +0500

    HOME_DIRECTORY

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,19 +67,19 @@ ssh $DOCKER_VM_HOST -i /tmp/id_rsa -o UserKnownHostsFile=/tmp/known_hosts << EOF
       cd \$DIR_NAME
       pwd
       # create a symlink for infra components to .env file (instead of copying it)
-      if [ -f $HOME_DIRECTORY/$REPOSITORY_ROOT_SECRETS/\$DIR_NAME/.env ];then
+      if [ -f \$HOME_DIRECTORY/$REPOSITORY_ROOT_SECRETS/\$DIR_NAME/.env ];then
         echo found .env file
         if [ -f .env ];then
           echo file or symlink exist
         else
           echo create symlink
-          ln -s $HOME_DIRECTORY/$REPOSITORY_ROOT_SECRETS/\$DIR_NAME/.env ./.env
+          ln -s \$HOME_DIRECTORY/$REPOSITORY_ROOT_SECRETS/\$DIR_NAME/.env ./.env
         fi
       fi
       # show if we found 'secrets.json'
-      if [ -f $HOME_DIRECTORY/$REPOSITORY_ROOT_SECRETS/\$DIR_NAME/secrets.json ];then
+      if [ -f \$HOME_DIRECTORY/$REPOSITORY_ROOT_SECRETS/\$DIR_NAME/secrets.json ];then
         echo found service secrets file
-        ls -la $HOME_DIRECTORY/$REPOSITORY_ROOT_SECRETS/\$DIR_NAME/secrets.json
+        ls -la \$HOME_DIRECTORY/$REPOSITORY_ROOT_SECRETS/\$DIR_NAME/secrets.json
       fi
       ls -la
       if [ "$ACTION" = "START" ];then
